d8a783e9dc644a97fb51e77835a78f81a34ce9cc
aazzioui <anas.azzioui@gmail.com>
2023-04-14 11:19:07 +0000

    Logging added and improved + stage one Done

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,9 +1,16 @@
 import os
 from src.utils.all_utils import read_yaml, create_directory, copy_file
 import argparse
-import pandas as pd
+#import pandas as pd
 from tqdm import tqdm # get a progress bar to check the progress in for loops
+import logging
 
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+#create_directory([log_dir])
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename = os.path.join(log_dir,"running_logs.log"), level = logging.INFO,
+                                            format = logging_str, filemode = 'a')
 
 def get_data(config_path):
     config = read_yaml(config_path)
@@ -29,5 +36,11 @@ if __name__ == '__main__':
     args.add_argument("--config","-c",default="config/config.yaml")
 
     parsed_args = args.parse_args()
+    try:
+        logging.info("\n")
+        logging.info(">>>>> stage 01 started")
+        get_data(config_path=parsed_args.config)
+        logging.info("stage 01 completed !! All the data are saved in local >>>>>")
 
-    get_data(config_path=parsed_args.config)
+    except Exception as e:
+        logging.exception(e)
